b8c5330d674fb61b8b58a958c3d53631086e3fe5
Kelve <kelvearagao@gmail.com>
2016-08-31 11:30:59 -0300

    cadastrando titulos.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# cobranca
+# Cobranca
+
+## Banco de teste
+
+URL para acessar o banco:
+
+    http://localhost:8080/h2-console/
+    
+Altere o JDBC URL para:
+
+    jdbc:h2:mem:testdb
---
+++ src/main/java/com/kelvearagao/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.kelvearagao.repository;
+
+import org.springframework.data.repository.Repository;
+
+import com.kelvearagao.model.Titulo;
+
+public interface Titulos extends Repository<Titulo, Long> {
+
+}
