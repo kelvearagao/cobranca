9648709949c7800ec6df71e9c2a0eb8b2f252227
Kelve <kelvearagao@gmail.com>
2016-08-31 23:54:47 -0300

    validando e editando titulos

--- src/main/java/com/kelvearagao/cobranca/controller/TituloController.java
+++ src/main/java/com/kelvearagao/cobranca/controller/TituloController.java
@@ -5,10 +5,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kelvearagao.cobranca.model.StatusTitulo;
 import com.kelvearagao.cobranca.model.Titulo;
@@ -17,6 +21,12 @@ import com.kelvearagao.cobranca.repository.Titulos;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+	
+	/**
+	 * Constante que representa o nome da tela de catastro de título.
+	 * 
+	 */
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
 	
 	/**
 	 * Repositório de titulos.
@@ -32,7 +42,9 @@ public class TituloController {
 	 */
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		
+		mv.addObject(new Titulo());
 				
 		return mv;
 	}
@@ -42,21 +54,47 @@ public class TituloController {
 	 * a tela de cadastro de título.
 	 * 
 	 * @param titulo
-	 * @return ModelAndView
+	 * @return String
 	 */
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo) {
-		titulos.save(titulo);
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+		if (errors.hasErrors()) {
+			return CADASTRO_VIEW;
+		}
 		
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem", "Título salvo com sucesso!");
+		titulos.save(titulo);
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+		
+		return "redirect:/titulos/novo";
+	}
+	/**
+	 * Retorna página de pesquisa de títulos.
+	 * 
+	 * @return
+	 */
+	@RequestMapping
+	public ModelAndView pesquisar() {
+		List<Titulo> todosTitulos = titulos.findAll();
+		ModelAndView mv = new ModelAndView("PesquisaTitulos");
+		
+		mv.addObject("titulos", todosTitulos);
 		
 		return mv;
 	}
 	
-	@RequestMapping
-	public String pesquisar() {
-		return "PesquisaTitulos";
+	/**
+	 * Recebe um titulo e retorna a página para editá-lo.
+	 * 
+	 * @param titulo
+	 * @return ModelAndView
+	 */
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		
+		mv.addObject(titulo);
+		
+		return mv;
 	}
 	
 	/**
